553eb3260ac0ed847046dacb494f6c06d3b16c45
Ananya Nair <an.nair1702@gmail.com>
2023-01-06 21:21:00 +0530

    Initial Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+import BooksList from './components/BooksList';
 
 function App() {
+
+  const [books, setBooks] = useState([        {
+    "Title": "Fantastic Beasts and Where to Find Them: The Original Screenplaye",
+    "Year": "2016",
+    "Type": "book",
+    "Poster": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1481542648i/29363501._SY180_.jpg"
+},
+{
+    "Title": "Fantastic Beasts: The Crimes of Grindelwald: The Original Screenplay Fantastic Beasts: The Crimes of Grindelwald: The Original Screenplay",
+    "Year": "2018",
+    "Type": "book",
+    "Poster": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1527724881i/39330961._SY180_.jpg"
+},
+{
+    "Title": "Fantastic Beasts: The Secrets of Dumbledore: The Original Screenplay Fantastic Beasts: The Secrets of Dumbledore: The Original Screenplay",
+    "Year": "2022",
+    "Type": "book",
+    "Poster": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1661082101i/41879350._SX120_.jpg"
+}]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <BooksList books={books} />
     </div>
   );
 }
