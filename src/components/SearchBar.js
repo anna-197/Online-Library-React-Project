8f55ad5b99f82fb398c44e43d7cce9e9b002b669
Ananya Nair <an.nair1702@gmail.com>
2023-01-25 19:05:03 +0530

    Fixed scroll

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,27 @@
 import React, { useState } from "react";
-import '../App.css';
-import Axios from "axios"
+import "../App.css";
+import Axios from "axios";
 
 const SearchBar = () => {
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
 
-    const[book,setBook] = useState("")
-    const[result, setResult] = useState([])
+  function handleChange(event) {
+    const book = event.target.value;
+    setBook(book);
+  }
 
-    function handleChange(event){
-
-        const book = event.target.value;
-        setBook(book);
-
-    }
-
-    function handleCLick(event) {
-      event.preventDefault();
-      Axios.get(
-        "https://www.googleapis.com/books/v1/volumes?q=" +
-          book +
-          "&key=AIzaSyCHYcFQLjiWJJpOxQyChKd9XRNh0wjgJ3w&maxResults=40"
-      ).then((response) => {
-        console.log(response);
-        setResult(response.data.items);
-      });
-    }
-
+  function handleCLick(event) {
+    event.preventDefault();
+    Axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+        book +
+        "&key=AIzaSyCHYcFQLjiWJJpOxQyChKd9XRNh0wjgJ3w&maxResults=40"
+    ).then((response) => {
+      console.log(response);
+      setResult(response.data.items);
+    });
+  }
 
   return (
     <>
@@ -38,24 +34,35 @@ const SearchBar = () => {
               placeholder="What book do you want to read?"
               onChange={handleChange}
             />
-            <button type="submit" class="searchButton" onClick={handleCLick} >
+            <button type="submit" class="searchButton" onClick={handleCLick}>
               Search
             </button>
           </div>
         </div>
       </div>
 
-
-
-
-
       <div className="result-container">
-        {result.map(each_book=>(
+        {result.map((each_book) => (
           <>
-            <div className="each-book-result">
-            <a href={each_book.volumeInfo.previewLink}>
-            <img src={each_book.volumeInfo.imageLinks && each_book.volumeInfo.imageLinks.smallThumbnail} alt="thumbnail"/>
-            </a>
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                class="card-img-top"
+                src={
+                  each_book.volumeInfo.imageLinks &&
+                  each_book.volumeInfo.imageLinks.smallThumbnail
+                }
+                alt="thumbnail"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{each_book.volumeInfo.title}</h5>
+
+                <a
+                  href={each_book.volumeInfo.previewLink}
+                  className="read-more"
+                >
+                  Read More
+                </a>
+              </div>
             </div>
           </>
         ))}
